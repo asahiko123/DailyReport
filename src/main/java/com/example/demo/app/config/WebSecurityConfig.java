dcbeda954fffaabea9a269fa9eba75f41e99bff3
hiroki asahina <evolution4532@gmail.com>
2021-10-01 12:49:24 +0900

    ログイン実装

--- src/main/java/com/example/demo/app/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/app/config/WebSecurityConfig.java
@@ -1,8 +1,52 @@
 package com.example.demo.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class WebSecurityConfig{
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	
+	@Autowired
+	private UserDetailsService userService;
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http
+		  .authorizeRequests()
+		     .antMatchers("/","/home").permitAll()
+		     .anyRequest().authenticated()
+		     .and()
+		  .formLogin()
+		     .loginPage("/login").permitAll()
+		     .and()
+		  .logout()
+		     .permitAll();
+		     
+	}
+	
+	@Override
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth
+		    .inMemoryAuthentication()
+		       .withUser("user").password("password").roles("USER");
+		
+	}
+	
+//	@Bean
+//	
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 }
