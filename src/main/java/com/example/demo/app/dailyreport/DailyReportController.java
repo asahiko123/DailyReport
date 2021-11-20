b4968e29c74f071689bed7fd9ac9e348187e44ef
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2021-11-20 21:40:14 +0900

    starturl変更

--- src/main/java/com/example/demo/app/dailyreport/DailyReportController.java
+++ src/main/java/com/example/demo/app/dailyreport/DailyReportController.java
@@ -19,7 +19,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
